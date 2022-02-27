641153decf4f2be914b2a516d52a822ec513648b
DESKTOP-0L313RJ\wnsgh <simyuseog@gmail.com>
2022-02-27 14:39:26 +0900

    테스트

--- randomblog/src/App.js
+++ randomblog/src/App.js
@@ -4,6 +4,8 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import CommonHeader from './header/commonHeader';
+import Main from './article/main';
+import RegistrationFrom from './loginFrom/RegistrationFrom';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
@@ -25,7 +27,7 @@ function App() {
       <Container maxWidth="md">
       <Grid container>
       <Grid item xs={12}>
-        <CommonHeader/>
+        <Main/>
         </Grid>
         </Grid>
       </Container>
---
+++ randomblog/src/article/main.js
@@ -0,0 +1,19 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+
+function Main() {
+  return (
+    <div className="wap">
+      <CssBaseline />
+      <Container maxWidth="md">
+      <Grid container>
+      <Grid item xs={12}>
+        </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+}
+
+export default Main;
