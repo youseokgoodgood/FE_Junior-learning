846f4de331b30913769d5ecd4fa9b3b8323b2eba
유석이노트북\wnsgh <wnsghrnt2586@gmail.com>
2022-04-24 22:11:17 +0900

    여기는 학습공간

--- nextjs/pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '../global.css'
-
-export default function YourApp({ Component, pageProps }) {
-  // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout || ((page) => page)
-  console.log(Component);
-  console.log(pageProps);
-  return getLayout(<Component {...pageProps} />)
-}
